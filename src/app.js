0a8f265a2ed4ef6615c8dc13b310962d7376f9f8
vikkis <baheryan.vika@gmail.com>
2017-10-31 09:46:14 +0200

    Get data from mongodb via mongoose

--- src/app.js
+++ src/app.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// bookAPI -> name of db to connect to
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const Book = require('./models/bookModel');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const bookRouter = express.Router();
+bookRouter.route('/Books')
+  .get((req, res) => {
+    let query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+
+    Book.find(query, (err, books) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(books);
+      }
+    });
+  });
+
+bookRouter.route('/Books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(book);
+      }
+    });
+  });
 
 app.use('/api', bookRouter);
 
